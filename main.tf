0179019c2395e58ea8235980a006332e9c1b5d33
sundarrajboobalan <73453612+sundarrajboobalan@users.noreply.github.com>
2025-11-13 10:29:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network3"
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
